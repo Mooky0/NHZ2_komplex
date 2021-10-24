6dbd5bf17a5430983fa6041da5fba0cbba028f9b
tothgabor <50456142+Mooky0@users.noreply.github.com>
2021-10-24 10:42:22 +0200

    Átváltás alakok közt
    tud alakot váltani. trig-ből alg-ba és vissza... kicsit messy a kód, de majd jobb lesz. nem túl ponotos a szöveg miatt... lehet majd maradni kell rad-ban. meg a test gyökök miatt... de jónak néz ki

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <math.h>
+#include <stdio.h>
+#define PI 3.141592654
 
 typedef struct komplex_algebrai{
     double Re, Im;
 } komplex_algebrai;
 
-typedef struct komplex_trig{
-    double r, fi; //Első kötben fokban tárolom a szöget, utána lehet váltani kell radiánra. inkább radiánban kezdek.
-} komplex_trig;
+typedef struct komplex{
+    double r, fi; //Első kötben fokban tárolom a szöget, utána lehet váltani kell radiánra.
+} komplex;
 
-void trig_to_algebrai(komplex_trig *trig, komplex_algebrai *alg){
+void algebrai_to_trig(komplex_algebrai *alg, komplex *trig){
     trig->r = sqrt(alg->Re*alg->Re + alg->Im * alg->Im);
-    trig->fi = atan(alg->Re / alg->Im);
+    trig->fi = (atan(alg->Im / alg->Re) * 180) / PI;
 
 }
 
-int main(){
+void trig_to_alg(komplex *trig, komplex_algebrai *alg){
+    alg->Re = trig->r * cos((trig->fi * PI) / 180);
+    alg->Im = trig->r * sin((trig->fi * PI) / 180);
+}
 
+int main(){
+    komplex_algebrai kiszam;
+    komplex szam;
+    scanf("%lf %lf", &kiszam.Re, &kiszam.Im);
+    algebrai_to_trig(&kiszam, &szam);
+    printf("r=%f fi=%f", szam.r, szam.fi);
+    trig_to_alg(&szam, &kiszam);
+    printf("Re= %f, Im=%f", kiszam.Re, kiszam.Im);
 
 
     return 0;
