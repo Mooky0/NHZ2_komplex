5f33b2e2546d8e0894696434922573d568ec90b1
tothgabor <50456142+Mooky0@users.noreply.github.com>
2021-10-31 22:03:37 +0100

    Főmenü draft (nem működik)
    +spec2 javított

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <math.h>
 #include <stdio.h>
-#define PI 3.141592654
 
 typedef struct komplex_algebrai{
+    int az;
     double Re, Im;
 } komplex_algebrai;
 
 typedef struct komplex{
+    int az;
     double r, fi; //Első kötben fokban tárolom a szöget, utána lehet váltani kell radiánra.
 } komplex;
 
@@ -16,7 +17,7 @@ typedef struct komplex{
  * A szög fokban értendő, kéőbb szintén válozni fog.*/
 void algebrai_to_trig(komplex_algebrai *alg, komplex *trig){
     trig->r = sqrt(alg->Re*alg->Re + alg->Im * alg->Im);
-    trig->fi = (atan(alg->Im / alg->Re) * 180) / PI;
+    trig->fi = (atan(alg->Im / alg->Re) * 180) / M_PI;
 
 }
 
@@ -25,8 +26,8 @@ void algebrai_to_trig(komplex_algebrai *alg, komplex *trig){
  * majd át kell alakítani, hogy rendesn stack-en dolgozzon, return value-vel.
  * A szög fokban értendő, kéőbb szintén válozni fog.*/
 void trig_to_alg(komplex *trig, komplex_algebrai *alg){
-    alg->Re = trig->r * cos((trig->fi * PI) / 180);
-    alg->Im = trig->r * sin((trig->fi * PI) / 180);
+    alg->Re = trig->r * cos((trig->fi * M_PI) / 180);
+    alg->Im = trig->r * sin((trig->fi * M_PI) / 180);
 }
 
 /*Összead kettő komplex számot algebrai alakban, és algebrai alakban tér vissza
@@ -41,23 +42,39 @@ komplex_algebrai osszead(komplex_algebrai szam1, komplex_algebrai szam2){
 }
 
 int main(){
-	/*test of conversion*/
-    // komplex_algebrai kiszam;
-    // komplex szam;
-    // scanf("%lf %lf", &kiszam.Re, &kiszam.Im);
-    // algebrai_to_trig(&kiszam, &szam);
-    // printf("r=%f fi=%f", szam.r, szam.fi);
-    // trig_to_alg(&szam, &kiszam);
-    // printf("Re= %f, Im=%f", kiszam.Re, kiszam.Im);
+	char opcio[1];
+    printf("OCIO: ");
+    scanf("%c", &opcio[1]);
+    while(opcio != 'Q'){
+        /* TODO lower to upper*/
+        switch (opcio[1]){
+        case 'I':
+            /* TODO Ide jön a beolvasás*/
+            printf("Opcio meg nem letezik(I)");
+            break;
+        case 'O':
+        case 'M':
+            /* TODO Ide jön a műveletes rész*/
+            printf("Opcio meg nem letezik(O/M)");
+            break;
+        case 'D':
+        case 'A':
+            /* TODO ábrázolás rész*/
+            printf("Opcio meg nem letezik(D/A)");
+            break;
+        case 'S':
+            /* TODO mentes txt-be*/
+            printf("Opcio meg nem letezik(S)");
+            break;
+        default:
+            printf("Ismeretlen input");
+            break;
+        }
+        printf("\nOCIO: ");
+        scanf("%c", &opcio[1]);
 
-
-	/*test of addition*/
-	komplex_algebrai szam1, szam2;
-    scanf("%lf %lf %lf %lf", &szam1.Re, &szam1.Im, &szam2.Re, &szam2.Im);
-    //printf("%f %f %f %f", szam1.Re, szam1.Im, szam2.Re, szam2.Im);
-    
-    komplex_algebrai osszeg = osszead(szam1, szam2);
-    printf("Re= %f, Im= %f", osszeg.Re, osszeg.Im);
+    }
+    printf("itt van.");
 
     return 0;
 }
